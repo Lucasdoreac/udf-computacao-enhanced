3c3901f3264ed0638ad9ab44f5f16a05e50c13df
Lucas Dórea Cardoso <160977944+Lucasdoreac@users.noreply.github.com>
2025-02-14 09:30:04 -0300

    Add Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '65ch',
+            color: '#333',
+            a: {
+              color: '#3182ce',
+              '&:hover': {
+                color: '#2c5282',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
